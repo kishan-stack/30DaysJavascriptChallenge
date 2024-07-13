23dabd46b7fca7f1c1f37ddea8537e28cbe0a441
kishan <varmajaykishan63@gmail.com>
2024-07-13 16:38:58 +0530

    first day challenge complete

---
+++ day1Challenge.js
@@ -0,0 +1,48 @@
+// Activity One: Variable declaration
+// Task 1:
+var variableUsingVar = 444;//number val
+console.log(variableUsingVar);
+
+// Task 2:
+let variableUsingLet = "SuperMan";//String val
+console.log(variableUsingLet);
+
+// Activity Two: Constant Declaration
+// Task 3:
+const variableUsingConst = true;//boolean val
+console.log(variableUsingConst);
+
+// Activity Three: Data Types
+// Task 4:
+let numberDatatype = 444;
+let stringdatatype = "superMan";
+let booleanDatatype = false;
+let objectDatatype = {
+    name:"batMan",
+    age:44
+}
+let arrayDatatype = [1,2,3,4,5]
+
+console.log(typeof numberDatatype)
+console.log(typeof stringdatatype)
+console.log(typeof booleanDatatype)
+console.log(typeof objectDatatype)
+console.log(typeof arrayDatatype)
+
+
+// Activity 4: Reassigning Variables
+// Task 5:
+let intialValueVariable = "this is the initial value";
+console.log(intialValueVariable);
+
+intialValueVariable = "this is the new value";
+console.log(intialValueVariable);
+
+
+// Activity 5: Reassigning Variables
+// Task 6:
+const constInitialVariable = "this is initial value of const ";
+console.log(constInitialVariable);
+
+constInitialVariable = "this is new value of const";
+console.log(constInitialVariable);
